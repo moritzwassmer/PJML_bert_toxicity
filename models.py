d35c41658dae6909b9db974c9c50b7a0637d25f8
Moritz Wassmer <moritz@wassmer-bw.de>
2023-11-19 13:37:18 +0100

    new PositionEmbedding class

--- models.py
+++ models.py
@@ -212,27 +212,33 @@ class BERTBase(nn.Module):
     class BERTEmbedding(torch.nn.Module):
 
         class PositionEmbedding(torch.nn.Module):
-            def __init__(self, embed_size, seq_len, device=DEVICE):
-                super().__init__()
-                n = 10000  # scalar for pos encoding
-                # create embedding matrix dim(seq_len  x embed_size)
-                self.embed_matrix = torch.zeros(seq_len, embed_size, device=device).float()
-                # positional encoding not to be updated while gradient descent
-                self.embed_matrix.require_grad = False
+            # TODO Docstring
+            """
+            has to be created only once
+
+            """
+
+            def create_embedding_matrix(self, embed_size, seq_len):
+                embed_matrix = torch.zeros(seq_len, embed_size, device=DEVICE).float()  # TODO DEVICE
+
+                # positional encoding not to be updated during gradient descent
+                embed_matrix.requires_grad = False
 
                 # compute embedding for each position in input
                 for position in range(seq_len):
-                    # run trough every component of embedding vector for each position with stride 2
-                    for c in range(0, embed_size, 2):
-                        # even
-                        self.embed_matrix[position, c] = math.sin(position / (n ** (2 * c / embed_size)))
-                        # uneven
-                        self.embed_matrix[position, c + 1] = math.cos(position / (n ** (2 * c / embed_size)))
+                    embed_matrix[position, ::2] = torch.sin(
+                        position / (10000  ** (2 * torch.arange(0, embed_size, 2) / embed_size)))
+                    embed_matrix[position, 1::2] = torch.cos(
+                        position / (10000  ** (2 * torch.arange(0, embed_size, 2) / embed_size)))
 
-                # self.embed_matrix =  embed_matrix.unsqueeze(0)
+                return embed_matrix
 
-            def forward(self, x):
-                return self.embed_matrix
+            def __init__(self, embed_size=EMBED_SIZE, seq_len=SEQ_LEN):
+                super().__init__()
+                self.pos_embedding = self.create_embedding_matrix(embed_size, seq_len)
+
+            def forward(self,x):
+                return self.pos_embedding
 
         def __init__(self, vocab_size, seq_len, embed_size=EMBED_SIZE, device=DEVICE):
             super().__init__()
